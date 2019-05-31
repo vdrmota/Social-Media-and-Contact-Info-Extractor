09cc45c47ad32ea353b9004a5b8b7afbcc1271ca
vdrmota <vojtech_drmota@college.harvard.edu>
2019-05-31 13:51:25 +0200

    style edits

--- src/crawlerConfig.js
+++ src/crawlerConfig.js
@@ -5,12 +5,12 @@ module.exports = {
     pageFunction: async (userData) => {
         // current page domain
         const domain = getDomain(window.location.href);
-        
-        try{
-            
+
+        try {
+
             // current html
             const html = $('body').html();
-            
+
             const result = {
                 url: window.location.href,
                 domain: domain,
@@ -18,25 +18,35 @@ module.exports = {
                 referrerUrl: userData.referrer,
                 html: html
             };
-            
+
             return result
-        
-        } 
-        catch(e){
+
+        } catch (e) {
             console.log("ERROR: " + e);
-            return {domain: domain, error: e.toString()};
+            return {
+                domain: domain,
+                error: e.toString()
+            };
         }
     },
 
-    gotoFunction: async ({ page, request }) => {
-    	await page.setRequestInterception(true)
-    	page.on('request', intercepted => {
-    	    const type = intercepted.resourceType();
-    		if(type === 'image' || type === 'stylesheet'){intercepted.abort();}
-    		else{intercepted.continue();}
-    	})
-    	console.log('Accessing ' + request.url);
-    	await Apify.utils.puppeteer.hideWebDriver(page);
-    	return await page.goto(request.url, {timeout: 200000});
+    gotoFunction: async ({
+        page,
+        request
+    }) => {
+        await page.setRequestInterception(true)
+        page.on('request', intercepted => {
+            const type = intercepted.resourceType();
+            if (type === 'image' || type === 'stylesheet') {
+                intercepted.abort();
+            } else {
+                intercepted.continue();
+            }
+        })
+        console.log('Accessing ' + request.url);
+        await Apify.utils.puppeteer.hideWebDriver(page);
+        return await page.goto(request.url, {
+            timeout: 200000
+        });
     }
 };
