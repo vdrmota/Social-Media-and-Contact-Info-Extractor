c5c6a15b13b1590d60216317bfd8c2b84ecc33e9
metalwarrior665 <lukaskrivka@gmail.com>
2020-05-08 10:28:54 +0200

    formatted to 4 spaced indent

--- src/main.js
+++ src/main.js
@@ -5,105 +5,105 @@ const PAGE_GOTO_TIMEOUT_SECS = 200;
 const WAIT_FOR_BODY_SECS = 60;
 
 Apify.main(async () => {
-  const input = await Apify.getValue('INPUT');
-  if (!input) throw new Error('There is no input!');
+    const input = await Apify.getValue('INPUT');
+    if (!input) throw new Error('There is no input!');
 
-  // Create RequestQueue
-  const requestQueue = await Apify.openRequestQueue();
+    // Create RequestQueue
+    const requestQueue = await Apify.openRequestQueue();
 
-  const requestList = await Apify.openRequestList('start-urls', input.startUrls);
+    const requestList = await Apify.openRequestList('start-urls', input.startUrls);
 
-  requestList.requests.forEach((req) => {
-    req.userData = {
-      depth: 0,
-      referrer: null,
-    };
-  });
+    requestList.requests.forEach((req) => {
+        req.userData = {
+            depth: 0,
+            referrer: null,
+        };
+    });
 
 
-  // Puppeteer options
-  const launchPuppeteerOptions = input.proxyConfig || {};
-  if (input.liveView) launchPuppeteerOptions.liveView = true;
-  launchPuppeteerOptions.stealth = true;
-  launchPuppeteerOptions.useChrome = true;
+    // Puppeteer options
+    const launchPuppeteerOptions = input.proxyConfig || {};
+    if (input.liveView) launchPuppeteerOptions.liveView = true;
+    launchPuppeteerOptions.stealth = true;
+    launchPuppeteerOptions.useChrome = true;
 
-  // Create the crawler
-  const crawlerOptions = {
-    requestList,
-    requestQueue,
-    handlePageFunction: async ({ page, request }) => {
-      console.log(`Processing ${request.url}`);
-
-      // Wait for body tag to load
-      await page.waitForSelector('body', {
-        timeout: WAIT_FOR_BODY_SECS * 1000,
-      });
-
-      // Set enqueue options
-      const linksToEnqueueOptions = {
-        page,
+    // Create the crawler
+    const crawlerOptions = {
+        requestList,
         requestQueue,
-        selector: 'a',
-        sameDomain: input.sameDomain,
-        urlDomain: helpers.getDomain(request.url),
-        depth: request.userData.depth,
-      };
+        handlePageFunction: async ({ page, request }) => {
+            console.log(`Processing ${request.url}`);
 
-      // Enqueue all links on the page
-      if (typeof input.maxDepth !== 'number' || request.userData.depth < input.maxDepth) {
-        await helpers.enqueueUrls(linksToEnqueueOptions);
-      }
+            // Wait for body tag to load
+            await page.waitForSelector('body', {
+                timeout: WAIT_FOR_BODY_SECS * 1000,
+            });
 
-      // Crawl HTML frames
-      let frameSocialHandles = {};
-      if (input.considerChildFrames) {
-        frameSocialHandles = await helpers.crawlFrames(page);
-      }
+            // Set enqueue options
+            const linksToEnqueueOptions = {
+                page,
+                requestQueue,
+                selector: 'a',
+                sameDomain: input.sameDomain,
+                urlDomain: helpers.getDomain(request.url),
+                depth: request.userData.depth,
+            };
 
-      // Generate result
-      const result = {};
-      result.html = await page.content();
-      result.depth = request.userData.depth;
-      result.referrerUrl = request.userData.referrer;
-      result.url = await page.url();
-      result.domain = await helpers.getDomain(result.url);
+            // Enqueue all links on the page
+            if (typeof input.maxDepth !== 'number' || request.userData.depth < input.maxDepth) {
+                await helpers.enqueueUrls(linksToEnqueueOptions);
+            }
 
-      // Extract and save handles, emails, phone numbers
-      const socialHandles = await Apify.utils.social.parseHandlesFromHtml(result.html);
+            // Crawl HTML frames
+            let frameSocialHandles = {};
+            if (input.considerChildFrames) {
+                frameSocialHandles = await helpers.crawlFrames(page);
+            }
 
-      // Merge frames with main
-      const mergedSocial = helpers.mergeSocial(frameSocialHandles, socialHandles);
-      Object.assign(result, mergedSocial);
+            // Generate result
+            const result = {};
+            result.html = await page.content();
+            result.depth = request.userData.depth;
+            result.referrerUrl = request.userData.referrer;
+            result.url = await page.url();
+            result.domain = await helpers.getDomain(result.url);
 
-      // Clean up
-      delete result.html;
+            // Extract and save handles, emails, phone numbers
+            const socialHandles = await Apify.utils.social.parseHandlesFromHtml(result.html);
 
-      // Store results
-      await Apify.pushData(result);
-    },
-    handleFailedRequestFunction: async ({ request }) => {
-      console.log(`Request ${request.url} failed 4 times`);
-    },
-    launchPuppeteerOptions,
-    gotoFunction: async ({ page, request }) => {
-      console.log(`Loading ${request.url}`);
+            // Merge frames with main
+            const mergedSocial = helpers.mergeSocial(frameSocialHandles, socialHandles);
+            Object.assign(result, mergedSocial);
 
-      // Block resources such as images and CSS files, to increase crawling speed
-      await Apify.utils.puppeteer.blockRequests(page);
+            // Clean up
+            delete result.html;
 
-      return page.goto(request.url, {
-        timeout: PAGE_GOTO_TIMEOUT_SECS * 1000,
-        waitUntil: 'domcontentloaded',
-      });
-    },
-  };
+            // Store results
+            await Apify.pushData(result);
+        },
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed 4 times`);
+        },
+        launchPuppeteerOptions,
+        gotoFunction: async ({ page, request }) => {
+            console.log(`Loading ${request.url}`);
 
-  // Limit requests
-  if (input.maxRequests) crawlerOptions.maxRequestsPerCrawl = input.maxRequests;
+            // Block resources such as images and CSS files, to increase crawling speed
+            await Apify.utils.puppeteer.blockRequests(page);
 
-  // Create crawler
-  const crawler = new Apify.PuppeteerCrawler(crawlerOptions);
+            return page.goto(request.url, {
+                timeout: PAGE_GOTO_TIMEOUT_SECS * 1000,
+                waitUntil: 'domcontentloaded',
+            });
+        },
+    };
 
-  // Run crawler
-  await crawler.run();
+    // Limit requests
+    if (input.maxRequests) crawlerOptions.maxRequestsPerCrawl = input.maxRequests;
+
+    // Create crawler
+    const crawler = new Apify.PuppeteerCrawler(crawlerOptions);
+
+    // Run crawler
+    await crawler.run();
 });
